754d608a014946e2dae83befc9b61b62dadee213
Himanshu Rana <himanshu.a.rana@gmail.com>
2018-12-01 22:22:02 +0530

    destructuring

---
+++ destructuring.js
@@ -0,0 +1,24 @@
+const turtle = {
+  name: 'Bob',
+  legs: 4,
+  shell: true,
+  type: 'amphibious',
+  meal: 10,
+  diet: 'berries'
+};
+
+// Bad Code
+// function feed(animal) {
+//   return `Feed ${animal.name} ${animal.meal} kilos of ${animal.diet}`;
+// }
+
+// Good Code
+function feed({name, meal, diet}) {
+  return `Feed ${name} ${meal} kilos of ${diet}`;
+}
+
+// OR
+function feed(animal) {
+  const {name, meal, diet} = animal;
+  return `Feed ${name} ${meal} kilos of ${diet}`;
+}
